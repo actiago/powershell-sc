841121d580e11f4089d1f46368df0575a8cc4b19
Tiago Amaral <tiagoamaralc@gmail.com>
2022-06-24 10:30:19 -0300

    Deleta arquivos movidos

--- Get-AdUsers.ps1
+++
@@ -1,4 +0,0 @@
-# Lista usuários do Active Directory e exporta para um arquivo
-# Para a obtenção de mais propriedades no filtro, use a documentação oficial: https://docs.microsoft.com/en-us/powershell/module/activedirectory/get-aduser?view=winserver2012-ps
-
-Get-ADUser -filter * -SearchBase 'DC=domain,DC=local' -Properties *|select name,PhysicalDeliveryOfficeName,CPF,Enabled,Mail,Created,LastLogonDate |export-csv C:\Temp\AD2.csv -Encoding UTF8
